bf6b123e0d1f2dd013baa989fc09326b44528b3a
Frank Baumann <frank@surveyoranalytics.com>
2023-08-03 03:03:55 -0500

    feat: add regions to home and style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { RegionContext } from "../contexts/RegionContext";
+import { calculateAverageNutrition } from "../utils";
+import './Home.css';
 
-export default function Home() {
+const Home: React.FC = () => {
+  const { regions } = useContext(RegionContext);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className="home-container">
+        <h2>Home</h2>
+        <p>Select a region to find out more</p>
+        {regions.map((region, index) => {
+          const { averageCalories, averageFat } = calculateAverageNutrition(
+            region.Fish
+          );
+          return (
+            <Link to={`/region/${region.Region}`} key={index}>
+              <div className="region-box">
+                <h2>{region.Region}</h2>
+                <p>Average Calories: {averageCalories.toFixed(2)}</p>
+                <p>Average Fat: {averageFat.toFixed(2)}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
